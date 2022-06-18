d120908ad403e7e7f0c0ac1bb3c418bfcebeb43a
MatiasUrielGluck <matiasugluck@gmail.com>
2022-06-18 08:08:09 -0300

    Created a navActions function to extract some code.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // alert("Hello! I am finishing the development of the page. Some things are incomplete, sorry for the inconvenience.");
 
-AOS.init();
+AOS.init(); // Scroll Animations Initialization
 
+// Hamb Menu
 const mobileBtn = document.getElementById('mobile-btn')
 nav = document.querySelector('nav')
 mobileBtnExit = document.getElementById('mobile-exit');
 
+// Nav Menu Buttons
 const homeSection = document.getElementById("home");
 const aboutSection = document.getElementById("about");
 const skillsSection = document.getElementById("skills");
 const portfolioSection = document.getElementById("portfolio");
 const contactSection = document.getElementById("contact");
 
+// Nav Sections
 const logoBtn = document.querySelector("#logo_btn");
 const homeBtn = document.querySelector("#home_btn");
 const aboutBtn = document.querySelector("#about_btn");
@@ -19,6 +22,16 @@ const skillsBtn = document.querySelector("#skills_btn");
 const portfolioBtn = document.querySelector("#portfolio_btn");
 const contactBtn = document.querySelector("#contact_btn");
 
+// Functions
+function navActions(button, section) {
+    nav.classList.remove('menu-btn');
+    nav.classList.add("inactive");
+    document.querySelector(".current").classList.remove("current");
+    button.parentElement.classList.add("current");
+    section.scrollIntoView({block: 'start', behavior: 'smooth'});
+}
+
+// Event Listeners
 mobileBtn.addEventListener('click', () => {
     nav.classList.add('menu-btn');
     nav.classList.remove("inactive");
@@ -33,42 +46,8 @@ logoBtn.addEventListener("click", () => {
     homeSection.scrollIntoView({block: 'start', behavior: 'smooth'});
 });
 
-homeBtn.addEventListener("click", () => {
-    nav.classList.remove('menu-btn');
-    nav.classList.add("inactive");
-    document.querySelector(".current").classList.remove("current");
-    homeBtn.parentElement.classList.add("current");
-    homeSection.scrollIntoView({block: 'start', behavior: 'smooth'});
-});
-
-aboutBtn.addEventListener("click", () => {
-    nav.classList.remove('menu-btn');
-    nav.classList.add("inactive");
-    document.querySelector(".current").classList.remove("current");
-    aboutBtn.parentElement.classList.add("current");
-    aboutSection.scrollIntoView({block: 'start', behavior: 'smooth'});
-});
-
-skillsBtn.addEventListener("click", () => {
-    nav.classList.remove('menu-btn');
-    nav.classList.add("inactive");
-    document.querySelector(".current").classList.remove("current");
-    skillsBtn.parentElement.classList.add("current");
-    skillsSection.scrollIntoView({block: 'start', behavior: 'smooth'});
-});
-
-portfolioBtn.addEventListener("click", () => {
-    nav.classList.remove('menu-btn');
-    nav.classList.add("inactive");
-    document.querySelector(".current").classList.remove("current");
-    portfolioBtn.parentElement.classList.add("current");
-    portfolioSection.scrollIntoView({block: 'start', behavior: 'smooth'});
-});
-
-contactBtn.addEventListener("click", () => {
-    nav.classList.remove('menu-btn');
-    nav.classList.add("inactive");
-    document.querySelector(".current").classList.remove("current");
-    contactBtn.parentElement.classList.add("current");
-    contactSection.scrollIntoView({block: 'start', behavior: 'smooth'});
-});
+homeBtn.addEventListener("click", () => { navActions(homeBtn, homeSection) });
+aboutBtn.addEventListener("click", () => { navActions(aboutBtn, aboutSection) });
+skillsBtn.addEventListener("click", () => { navActions(skillsBtn, skillsSection) });
+portfolioBtn.addEventListener("click", () => { navActions(portfolioBtn, portfolioSection) });
+contactBtn.addEventListener("click", () => { navActions(contactBtn, contactSection) });
